84559cffe5e4b70763bfc99d409cf38e87caf0a8
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-05-14 20:43:54 +0530

    Custom Mocking Logic

--- src/util/io.test.js
+++ src/util/io.test.js
@@ -3,11 +3,22 @@ import { promises as fs } from "fs";
 import writeData from "./io";
 
 vi.mock("fs");
+vi.mock("path", () => {
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length - 1];
+      },
+    },
+  };
+});
 
 it("should execute the writeFile() method", () => {
   const testData = "Test";
   const testFileName = "test.txt";
   // return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
   writeData(testData, testFileName);
-  expect(fs.writeFile).toBeCalled();
+  // expect(fs.writeFile).toBeCalled();
+
+  expect(fs.writeFile).toBeCalledWith(testFileName, testData);
 });
