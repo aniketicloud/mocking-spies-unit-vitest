666b9c1d1363a6ba70a8607951ff12dd52f2a218
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-05-14 15:21:56 +0530

    Getting Started with Mocks & Automocking

--- src/util/io.test.js
+++ src/util/io.test.js
@@ -1,8 +1,13 @@
-import { it, expect } from "vitest";
+import { it, expect, vi } from "vitest";
+import { promises as fs } from "fs";
 import writeData from "./io";
+
+vi.mock("fs");
 
 it("should execute the writeFile() method", () => {
   const testData = "Test";
   const testFileName = "test.txt";
-  return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+  // return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+  writeData(testData, testFileName);
+  expect(fs.writeFile).toBeCalled();
 });
